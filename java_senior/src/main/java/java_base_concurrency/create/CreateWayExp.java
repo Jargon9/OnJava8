a2e34e3311a48064cbe2b586d2195086cd3758df
Jargon96 <jargon9@pku.edu.cn>
2022-06-05 00:11:09 +0800

    Concurrency exp

---
+++ java_senior/src/main/java/java_base_concurrency/create/CreateWayExp.java
@@ -0,0 +1,20 @@
+package java_base_concurrency.create;
+
+public class CreateWayExp {
+    public static void main(String[] args) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("start" + Thread.currentThread().getName());
+            }
+        };
+        Thread test = new Thread(task);
+        test.setName("test");
+        // run 用于主线程，打出来的名称是main,和多线程没有关系。
+        test.run();
+
+        // start打出来是 test
+    }
+
+
+}

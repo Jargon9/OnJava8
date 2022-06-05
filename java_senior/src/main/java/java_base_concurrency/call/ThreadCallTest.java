3deaa8420435a48227a526c214a855e392195b20
Jargon96 <jargon9@pku.edu.cn>
2022-06-05 10:35:26 +0800

    Add join exp

---
+++ java_senior/src/main/java/java_base_concurrency/call/ThreadCallTest.java
@@ -0,0 +1,16 @@
+package java_base_concurrency.call;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class ThreadCallTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ThreadCall test = new ThreadCall();
+        FutureTask<String> ft = new FutureTask<>(test);
+        new Thread(ft).start();
+        System.out.println("线程开始");
+
+        System.out.println(ft.get());
+
+    }
+}
